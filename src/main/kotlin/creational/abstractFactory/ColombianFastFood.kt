2acd2b713c4fe1aa5a4ca47520d4d63ba434b0f1
Samuel Estepa <samuel.estepa@gmail.com>
2022-01-03 13:18:34 -0500

    se retiran metodos que no se habian implementado en la clase ColombianFastFood

--- src/main/kotlin/creational/abstractFactory/ColombianFastFood.kt
+++ src/main/kotlin/creational/abstractFactory/ColombianFastFood.kt
@@ -1,7 +1,6 @@
 package creational.abstractFactory
 
 import creational.factoryMethod.hamburger.HamburgerFactory
-import creational.factoryMethod.hamburger.IHamburger
 import creational.factoryMethod.hamburger.IHamburgerCreator
 import creational.factoryMethod.pizza.IPizzaCreator
 import creational.factoryMethod.pizza.PizzaFactory
@@ -9,15 +8,4 @@ import creational.factoryMethod.pizza.PizzaFactory
 class ColombianFastFood: IFastFood {
     override fun createPizza() = PizzaFactory().create(IPizzaCreator.EType.COLOMBIANA)
     override fun createHamburger() = HamburgerFactory().create(IHamburgerCreator.EType.COLOMBIANA)
-    override fun createHotDog(): IHamburger {
-        TODO("Not yet implemented")
-    }
-
-    override fun createChickenFry(): IHamburger {
-        TODO("Not yet implemented")
-    }
-
-    override fun createFrechFry(): IHamburger {
-        TODO("Not yet implemented")
-    }
 }
